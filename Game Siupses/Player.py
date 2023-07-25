e6868481bcf5b56b5164e7be70f0109adb1e88dd
maminike <kapnolol@gmail.com>
2023-07-25 15:42:51 +0200

    zrobilem siupe, ze gracz jest gitem

--- Game Siupses/Player.py
+++ Game Siupses/Player.py
@@ -15,35 +15,43 @@ class Player:
         self.onGround = 0
         self.isCrouching = 0
         self.isJumping = 0
+        self.direction_force = 0
 
     def update(self):
         keys = pygame.key.get_pressed()
-        if not self.isJumping:
+        if not self.isJumping and not self.isCrouching:
             if keys[pygame.K_d]:
                 self.rectangle.centerx += 10
             if keys[pygame.K_a]:
-
                 self.rectangle.centerx -= 10
 
         # generalnie grav musi być przed jump już nie pamiętam dla czego
         self.grav()
         self.jump(keys)
+        if not self.onGround:
+            self.rectangle.x += self.direction_force
 
-        #print("Crouch: "+ str(self.isCrouching))
-        #print("Ground: " + str(self.onGround))
+        # print("Crouch: "+ str(self.isCrouching))
+        # print("Ground: " + str(self.onGround))
+
         print("Jumping: " + str(self.isJumping))
         self.screen.blit(self.surface, self.rectangle)
 
     def jump(self, keys):
         # jumpington
-        if keys[pygame.K_w]:
+        if keys[pygame.K_SPACE] and self.onGround:
             # jeżeli w jest klikniete to sie charguje siła skoka
             self.isJumping = False
             self.isCrouching = True
-            self.onGround = True
+            self.direction_force = 0
             if self.jump_force <= 25:
                 self.jump_force += 0.75
-        elif self.jump_force != 0:
+            if keys[pygame.K_a]:
+                self.direction_force = -8
+            if keys[pygame.K_d]:
+                self.direction_force = 8
+
+        elif self.jump_force != 0 and self.onGround:
             self.isCrouching = False
             self.isJumping = True
             self.onGround = False
@@ -52,18 +60,13 @@ class Player:
             self.jump_force = 0  # niesamowity świat chuja/ zerowanie forca skoka
 
     def grav(self):
-        # jak to sie dzieje na świecie przedmioty grawitacyjnie spadaja na dół
-        # prędkość z jaką spada zwiększa się cały czas
         self.gravity += 1
 
-        # jeżeli jest pod miejscem na którym sie zrespił no to go wykurwia tam znowu zjeba jebanego
-        # grawitacja musi być większa od zera bo jak robi skoka to jest ujemna co nie
-        # inaczej by se programik pomyślał że nie powinien spadać w góre i by wyzerował śmieć
-        # todo naprawić te grawitacje huja wartą
         if self.rectangle.y + self.rectangle.height >= self.resp_y and self.gravity >= 0:
             self.gravity = 0
-        # print(self.gravity)  # możesz zobaczyć jak ta grawitacja siupuje
+            # print(self.gravity)  # możesz zobaczyć jak ta grawitacja siupuje
             self.onGround = True
             self.isJumping = False
+
         # tutaj dokonuje sie spadanie(lub skakanie(srakanie hahaha)) właściwe, po prostu dodaje sie grawitacja do y
         self.rectangle.y += self.gravity
